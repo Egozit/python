3c5881260e3ee6c7b41a6a9e99ee7d625c7f3b2f
Egozit <egor_petrov97_kurkino@mail.ru>
2018-09-30 18:57:59 +0300

    les01 perfect code

---
+++ Lesson2/les02.py
@@ -0,0 +1,58 @@
+﻿# EASY
+#
+# task 1
+#
+# import random
+#
+# num = random.randint(-1000, 1000)
+#
+# idx = 0
+# data = str(num)
+# print(data)
+#
+# while idx < len(data):
+#     print(idx, data[idx])
+#     idx += 1
+#
+# Better version
+# import random
+#
+# num = random.randint(-1000, 1000)
+# print(num)
+#
+# for idx, char in enumerate(str(num)):
+#     print(idx, char)
+#
+# task 2
+#
+# a = input('enter a ')
+# b = input('enter b ')
+#
+# a, b = b, a
+#
+# print(a, b)
+#
+# task 3
+#
+# age = input('enter age ')
+#
+# message = 'success' if int(age) >= 18 else 'denied'
+#
+# print(message)
+#
+# HARD
+#
+# a = float('infinity')
+#
+# print(a)
+#
+# LESSON NUMBER TWO
+#
+# strings
+#
+# string1 = ''
+# string2 = ""
+# string3 = '''
+# Hello
+# world
+# '''
